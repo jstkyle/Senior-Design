5bfde3e859692ac171ebfb7d61093da23f67c908
jstkyle <jstkyle1220@gmail.com>
2022-04-14 23:17:43 -0400

    C

---
+++ Raspi_Software/control.py
@@ -0,0 +1,13 @@
+import time
+import serial
+               
+ser = serial.Serial(            
+    port='/dev/serial0',
+    baudrate = 115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+
+ser.write('p'.encode())
