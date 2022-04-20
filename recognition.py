e252db23de107e669745745571a61673f5096691
jstkyle <jstkyle1220@gmail.com>
2022-04-20 00:06:29 -0400

    C

--- recognition.py
+++ recognition.py
@@ -101,7 +101,7 @@ def is_entry(cnts):
         rect1 = cv2.boundingRect(cnts[0])
         rect2 = cv2.boundingRect(cnts[1])
 
-        if rect1[3] > rect2[3] * 0.99 and rect1[3] < rect2[3] * 1.01:
+        if rect1[3] > rect2[3] * 0.983 and rect1[3] < rect2[3] * 1.017:
             return True
     
     return False
