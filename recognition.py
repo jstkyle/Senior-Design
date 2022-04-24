20058e07f0316b48d83711cbdb972800bdecffd2
jstkyle <jstkyle1220@gmail.com>
2022-04-24 19:52:45 -0400

    C

--- recognition.py
+++ recognition.py
@@ -23,7 +23,7 @@ def detect_blue(frame):
 def detect_green(frame):
     # filter for green
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_green = np.array([40, 40, 70])
+    lower_green = np.array([40, 70, 70])
     upper_green = np.array([90, 255, 255])
     mask = cv2.inRange(hsv, lower_green, upper_green)
 
