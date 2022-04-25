b22286a22268cda949a435680a1ea56646b931f8
jstkyle <jstkyle1220@gmail.com>
2022-04-25 19:19:43 -0400

    C

--- test_webcam_circle.py
+++ test_webcam_circle.py
@@ -42,11 +42,14 @@ while True:
     center, radius = rec.detect_circle_green(blur)
     point = (0,0)
     dir = rec.park_dir(frame, center)
+    '''
     print(dir[0])
     print(center, dir)
     if center != point:
         print(f"X diff: {dir[0]}")
         print(f"Y diff: {dir[1]}")
+        '''
+    print(radius)
     cv2.circle(frame, np.int0(center), 2, (0,0,255), thickness=10)
     cv2.circle(frame, (mid_x, mid_y), 2, (255,0,0), thickness=10)
     # End time
