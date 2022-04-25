3537fefe7ee3cbd63aa528d46b007536954bf597
jstkyle <jstkyle1220@gmail.com>
2022-04-25 17:31:50 -0400

    C

--- run_v2.0.py
+++ run_v2.0.py
@@ -222,7 +222,7 @@ class myThread(threading.Thread):
             try:
                 green_center = self.green_center
                 x_diff = self.green_dir[0]
-                print(self.radius)
+                #print(self.radius)
                 #print(f"x_diff: {x_diff}, center: {green_center}")
                 if self.radius > 17.5:
                     #print(self.radius)
