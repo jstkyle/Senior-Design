661d7273637712e457feb0a9ea9fbd7bdc2094ba
jstkyle <jstkyle1220@gmail.com>
2022-04-25 17:23:36 -0400

    C

--- recognition.py
+++ recognition.py
@@ -5,7 +5,7 @@ import numpy as np
 def detect_yellow(frame):
     # filter for yellow
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_yellow = np.array([20, 30, 80])
+    lower_yellow = np.array([20, 30, 60])
     upper_yellow = np.array([50, 255, 255])
     mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
 
