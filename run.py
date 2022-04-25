e98f66609fc6bf7600c8a4ecb625559855ec719b
jstkyle <jstkyle1220@gmail.com>
2022-04-25 01:39:34 -0400

    C

--- run.py
+++ run.py
@@ -252,7 +252,7 @@ class myThread(threading.Thread):
             try:
                 green_center = self.green_center
                 x_diff = self.green_dir[0]
-                #print(f"x_diff: {x_diff}, center: {green_center}")
+                print(f"x_diff: {x_diff}, center: {green_center}")
                 if self.radius > 21:
                     #print(self.radius)
                     break
