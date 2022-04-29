498de3c546da90667930092c32804c9359535be0
jstkyle <jstkyle1220@gmail.com>
2022-04-29 10:29:07 -0400

    C

--- recognition.py
+++ recognition.py
@@ -76,7 +76,7 @@ def detect_circle(blur):
 def detect_green(frame):
     # filter for green
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_green = np.array([40, 50, 50])
+    lower_green = np.array([43, 50, 50])
     upper_green = np.array([90, 255, 255])
     mask = cv2.inRange(hsv, lower_green, upper_green)
 
